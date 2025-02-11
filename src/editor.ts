d4387042e6f2e6dffcfd9f22ee8d50deea7856ed
Nolan Manas <Nolan.Manas@RubicoTech.in>
2025-02-11 13:12:32 +0530

    TTY-#LBBR1hzB add: handle key event for cursor navigation

--- src/editor.ts
+++ src/editor.ts
@@ -1,6 +1,7 @@
 import readLine, { createInterface, Key } from "node:readline";
 import fs from "node:fs";
 import { ReadStream, WriteStream } from "node:tty";
+import { KeyBoardKey, NavigationKeys } from "./constants/keybroad";
 import { printOnFile } from "./utils/printOnFIle";
 
 export class Editor {
@@ -34,16 +35,97 @@ export class Editor {
   }
 
   handleKeyEvents(str: string, key: Key) {
-    printOnFile(`${JSON.stringify(this.cursorPosition)}`);
     if (key.ctrl && key.name === "c") {
       this.clearScreen(this.writeStream);
       process.exit(0);
     }
 
-   
+    // handle cursor position handling
+    if (!!key?.name && NavigationKeys.includes(key?.name)) {
+      this.handleCursorNavigation(key?.name as KeyBoardKey);
+    }
+    printOnFile(`${JSON.stringify(this.cursorPosition)}`);
   }
 
-  
+  handleCursorNavigation(keyName: KeyBoardKey) {
+    switch (keyName) {
+      case KeyBoardKey.ARROW_UP:
+        if (this.buffer?.[this.cursorPosition.row - 1]) {
+          if (
+            this.buffer?.[this.cursorPosition.row - 1].length >=
+            this.cursorPosition.column
+          ) {
+            this.cursorPosition.row -= 1;
+            this.writeStream?.cursorTo(
+              this.cursorPosition.column,
+              this.cursorPosition.row
+            );
+          } else {
+            this.cursorPosition.row -= 1;
+            this.cursorPosition.column =
+              this.buffer[this.cursorPosition.row].length - 1;
+            this.writeStream?.cursorTo(
+              this.cursorPosition.column,
+              this.cursorPosition.row
+            );
+          }
+        }
+
+        break;
+      case KeyBoardKey.ARROW_DOWN:
+        if (this.buffer?.[this.cursorPosition.row + 1]) {
+          if (
+            this.buffer?.[this.cursorPosition.row + 1].length >=
+            this.cursorPosition.column
+          ) {
+            this.cursorPosition.row += 1;
+            this.writeStream?.cursorTo(
+              this.cursorPosition.column,
+              this.cursorPosition.row
+            );
+          } else {
+            this.cursorPosition.row += 1;
+            this.cursorPosition.column =
+              this.buffer[this.cursorPosition.row].length - 1;
+            this.writeStream?.cursorTo(
+              this.cursorPosition.column,
+              this.cursorPosition.row
+            );
+          }
+        }
+
+        break;
+      case KeyBoardKey.ARROW_LEFT:
+        if (
+          this.buffer?.[this.cursorPosition.row] &&
+          this.buffer?.[this.cursorPosition.row]?.[
+            this.cursorPosition.column - 1
+          ]
+        ) {
+          this.cursorPosition.column -= 1;
+          this.writeStream?.cursorTo(
+            this.cursorPosition.column,
+            this.cursorPosition.row
+          );
+        }
+        break;
+      case KeyBoardKey.ARROW_RIGHT:
+        if (
+          this.buffer?.[this.cursorPosition.row] &&
+          this.buffer?.[this.cursorPosition.row]?.[
+            this.cursorPosition.column + 1
+          ]
+        ) {
+          this.cursorPosition.column += 1;
+          this.writeStream?.cursorTo(
+            this.cursorPosition.column,
+            this.cursorPosition.row
+          );
+        }
+        break;
+    }
+  }
+
   // read given file add file content in the buffer
   async handleFileInput(filePath: string): Promise<string[]> {
     const buffer: string[] = [];
