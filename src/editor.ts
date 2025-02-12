7d13b3eb90e38620955eccf5945a990eb541b839
drakemohit <drake.mohit@rubicotech.in>
2025-02-12 14:10:23 +0530

    TTY-#cC6QfKn9 add: tab key handling

--- src/editor.ts
+++ src/editor.ts
@@ -1,8 +1,8 @@
 import readLine, { createInterface, Key } from "node:readline";
 import fs from "node:fs";
 import { ReadStream, WriteStream } from "node:tty";
-import { KeyBoardKey, NavigationKeys } from "./constants/keybroad";
-import { printOnFile } from "./utils/printOnFIle";
+import { KeyBoardKey, NavigationKeys } from "./constants/keyboard";
+import { printOnFile } from "./utils/printOnFile";
 
 export class Editor {
   constructor() {}
@@ -66,13 +66,51 @@ export class Editor {
     if (
       !!key?.sequence &&
       !!key.name &&
-      !["return", "space", "backspace", "tab"].includes(key.name) &&
+      ![
+        KeyBoardKey.RETURN,
+        KeyBoardKey.SPACE,
+        KeyBoardKey.BACKSPACE,
+        KeyBoardKey.TAB,
+      ].includes(key.name as KeyBoardKey) &&
       !NavigationKeys.includes(key.name) &&
       this.windowSize[0] > this.buffer[this.cursorPosition.row].length
     ) {
       this.handleCharacterInput(key);
     }
+
+    // handle TAB key
+    if (this.windowSize[0] > this.buffer[this.cursorPosition.row].length) {
+      if (!!key?.name && key.name === KeyBoardKey.TAB) {
+        this.handleTabKey();
+      }
+    }
+
     printOnFile(`${JSON.stringify(this.cursorPosition)}`);
+  }
+
+  handleTabKey() {
+    const tabSize = 2;
+    const currentLine = this.buffer[this.cursorPosition.row];
+
+    const newLine =
+      currentLine.slice(0, this.cursorPosition.column) +
+      " ".repeat(tabSize) +
+      currentLine.slice(this.cursorPosition.column);
+
+    // Update the buffer with the new line
+    this.buffer[this.cursorPosition.row] = newLine;
+    this.cursorPosition.column += tabSize;
+
+    // Redraw the current line
+    this.writeStream?.cursorTo(0, this.cursorPosition.row);
+    this.writeStream?.clearLine(1);
+    this.writeStream?.write(newLine);
+
+    // Move the cursor to the new position
+    this.writeStream?.cursorTo(
+      this.cursorPosition.column,
+      this.cursorPosition.row,
+    );
   }
 
   handleCursorNavigation(keyName: KeyBoardKey) {
@@ -152,7 +190,10 @@ export class Editor {
     }
   }
 
-  // read given file add file content in the buffer
+  /**
+   * read given file add file content in the buffer
+   * @param filePath
+   */
   async handleFileInput(filePath: string): Promise<string[]> {
     const buffer: string[] = [];
     if (!fs.existsSync(filePath)) {
@@ -181,29 +222,34 @@ export class Editor {
     });
   }
 
-  //Handles the input of a single character in the text editor.
-  //This method is responsible for inserting a character into the current row of the text buffer at the cursor's position, moving the cursor forward, and redisplaying the updated buffer on the screen.
+  /**
+   * Handles the input of a single character in the text editor.
+   * This method is responsible for inserting a character into the current row of the text buffer
+   * at the cursor's position, moving the cursor forward, and re-displaying the updated buffer on the screen.
+   */
   async handleCharacterInput(key: Key) {
     const currentRow = this.buffer[this.cursorPosition.row];
 
     if (currentRow) {
-      //Insert the character at the cursor position in the row
+      // Insert the character at the cursor position in the row
       this.buffer[this.cursorPosition.row] =
         currentRow.slice(0, this.cursorPosition.column) +
         key.sequence +
         currentRow.slice(this.cursorPosition.column);
 
-      // // Move the cursor forward after the inserted character
+      // Move the cursor forward after the inserted character
       this.cursorPosition.column += 1;
 
-      //Redisplay the buffer with the new content
+      // Re-display the buffer with the new content
       this.clearScreen(this.writeStream);
-      //sending the row and colum for giving the right position of cursor for the next time
+      // sending the row and colum for giving the right position of cursor for the next time
       this.display(this.cursorPosition.row, this.cursorPosition.column);
     }
   }
 
-  // clear terminal screen
+  /**
+   * clear terminal screen
+   */
   clearScreen(
     writeStream:
       | (WriteStream & {
